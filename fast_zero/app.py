f006aa04c644bdb115409a23489afa5b0a324566
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-02-05 10:44:33 -0300

    Aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/mundo', status_code=200, response_class=HTMLResponse)
+def ola_mundo_html():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title> Tarefa 1 </title>
+</head>
+<body>
+    <h1> Olá Mundo!</h1>
+    <p> Estevam lindão </p>
+</body>
+</html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,37 @@
+import textwrap
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
+def test_root():
     client = TestClient(app)
 
     response = client.get('/')
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_mundo():
+    client = TestClient(app)
+
+    response = client.get('/mundo')
+
+    assert response.status_code == 200
+    expected_content = """<!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <title> Tarefa 1 </title>
+        </head>
+        <body>
+            <h1> Olá Mundo!</h1>
+            <p> Estevam lindão </p>
+        </body>
+        </html>"""
+
+    assert ''.join(response.content.decode('utf-8').split()) == ''.join(
+        textwrap.dedent(expected_content).strip().split()
+    )
