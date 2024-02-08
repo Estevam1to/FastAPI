54b4ae108e9abab4a42afd8462bfdcf28276d235
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-02-08 10:51:25 -0300

    Implementando teste e função de delete users

--- tests/test_app.py
+++ tests/test_app.py
@@ -78,3 +78,10 @@ def test_update_user(client):
         'email': 'vascodagama@exemplo.com',
         'id': 1,
     }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'User deleted'}
