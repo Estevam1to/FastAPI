f266831432c1052b8f032a5e61718774181fbb39
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-02-21 12:22:42 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -31,6 +31,18 @@ def test_mundo(client):
     )
 
 
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in token
+    assert 'token_type' in token
+
+
 def test_create_user(client):
     response = client.post(
         '/users/',
@@ -77,40 +89,29 @@ def test_read_users_with_users(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
-            'username': 'estevamm',
-            'email': 'estevamm@example.com',
+            'username': 'bob',
+            'email': 'bob@example.com',
             'password': 'mynewpassword',
         },
     )
     assert response.status_code == 200
     assert response.json() == {
-        'username': 'estevamm',
-        'email': 'estevamm@example.com',
+        'username': 'bob',
+        'email': 'bob@example.com',
         'id': 1,
     }
 
 
-# def test_read_user(client, user):
-#     response = client.get('/user/1')
-
-#     assert response.status_code == 200
-#     assert response.json() == {
-#         'user': {
-#             'id': 1,
-#             'username': 'vasco',
-#             'email': 'vascodagama@exemplo.com',
-#             'password': 'vascocampeao',
-#         }
-#     }
-
-
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
-
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
     assert response.status_code == 200
     assert response.json() == {'message': 'User deleted'}
 
