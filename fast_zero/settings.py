9d8555fd8b105b3cd497296a4031edea870a3bf8
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-03-12 15:14:05 -0300

    Alterando forma de leitura de variáveis

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -1,12 +1,12 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env', env_file_encoding='utf-8', extra='ignore'
     )
 
-    DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DATABASE_URL: str = os.getenv('DATABASE_URL')
+    SECRET_KEY:str = os.getenv('SECRET_KEY')
+    ALGORITHM: str = os.getenv('ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
